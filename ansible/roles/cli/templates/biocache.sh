5eae960734abb4b419d887d321410a997a77f90c
Dave Martin <moyesyside@gmail.com>
2014-04-03 15:26:54 +1100

    large refactor to support ubuntu installs. Apps all starting up on CentOS but not functionality tested.

--- ansible/roles/cli/templates/biocache.sh
+++ ansible/roles/cli/templates/biocache.sh
@@ -1,1 +1,1 @@
-java -cp /usr/lib/biocache:/usr/lib/biocache/{{biocache_store}} -Xms2g -Xmx2g au.org.ala.util.CommandLineTool $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+java -cp /usr/lib/biocache:/usr/lib/biocache/{{biocache_store}} -Xms2g -Xmx2g au.org.ala.util.CommandLineTool
