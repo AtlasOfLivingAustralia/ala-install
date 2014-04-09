d5b6394f44aa82451079d84f646e200caafbd2a8
Dave Martin <moyesyside@gmail.com>
2014-04-09 20:53:09 +1000

    fixes for SDS and APPD installs

--- ansible/roles/cli/templates/biocache.sh
+++ ansible/roles/cli/templates/biocache.sh
@@ -1,1 +1,1 @@
-java -cp /usr/lib/biocache:/usr/lib/biocache/{{biocache_store}} -Xms2g -Xmx2g au.org.ala.util.CommandLineTool
+java -cp /usr/lib/biocache:/usr/lib/biocache/{{biocache_store}} -Xms2g -Xmx2g au.org.ala.biocache.cmd.CommandLineTool
