b63d1762c0d8d3949f0f8493d063bfb4daa16898
Dave Martin <djtfmartin@gmail.com>
2014-09-05 15:18:30 +1000

    update of collectory, name matching, hub, solr
    update of collectory, name matching, hub, solr versions

--- ansible/roles/nameindexer/templates/nameindexer.sh
+++ ansible/roles/nameindexer/templates/nameindexer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec java -Dlog4j.configuration=file:/usr/lib/nameindexer/log4j.xml -Dfile.encoding=UTF8 -Dactors.corePoolSize=8 -Dactors.maxPoolSize=16 -Dactors.minPoolSize=8 -Djava.util.Arrays.useLegacyMergeSort=true {{nameindexer_opts|default('-Xmx1g -Xms1g')}} -jar $0 "$@"
+exec java -Dlog4j.configuration=file:/usr/lib/nameindexer/log4j.xml -Dfile.encoding=UTF8 -Djava.util.Arrays.useLegacyMergeSort=true {{nameindexer_opts|default('-Xmx1g -Xms1g')}} -jar $0 "$@"
 
 
