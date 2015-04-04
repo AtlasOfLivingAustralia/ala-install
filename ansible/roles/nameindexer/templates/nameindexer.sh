e56e9b263d159fa5f01d1beaf53e533db10d8157
Dave Martin <djtfmartin@gmail.com>
2015-04-04 22:13:45 +1100

    handling different indexes, biocache config properties
    handling different indexes with support to switch between index types
    for deployments, biocache config properties, removed redundant biocache
    role

--- ansible/roles/nameindexer/templates/nameindexer.sh
+++ ansible/roles/nameindexer/templates/nameindexer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec java -Dlog4j.configuration=file:/usr/lib/nameindexer/log4j.xml -Dfile.encoding=UTF8 -Djava.util.Arrays.useLegacyMergeSort=true {{nameindexer_opts|default('-Xmx1g -Xms1g')}} -jar $0 "$@"
+exec java -Dlog4j.configuration=file:/usr/lib/nameindexer/log4j.xml -Dfile.encoding=UTF8 -Djava.util.Arrays.useLegacyMergeSort=true {{ nameindexer_opts | default('-Xmx1g -Xms1g') }} -jar $0 "$@"
 
 
