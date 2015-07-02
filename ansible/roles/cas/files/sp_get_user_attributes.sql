2cc5e19c359d77ccdbbdb704123f0934dbaab484
mbohun <martin.bohun@gmail.com>
2015-07-02 13:07:16 +1000

    added sp_create_user used by ala-cas-2.0 to create/register ALA users when they login with Facebook/Twitter/Google/etc.

--- ansible/roles/cas/files/sp_get_user_attributes.sql
+++ ansible/roles/cas/files/sp_get_user_attributes.sql
@@ -21,3 +21,33 @@ select username, group_concat(ur.role_id) as 'authorities' from users u, user_ro
 end
 //
 delimiter ;
+
+
+DROP PROCEDURE IF EXISTS sp_create_user;
+delimiter //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create_user`(
+`email`              varchar(255),
+`firstname`          varchar(255),
+`lastname`           varchar(255),
+`password`           varchar(255),
+`city`               varchar(255),
+`organisation`       varchar(255),
+`primaryUserType`    varchar(255),
+`secondaryUserType`  varchar(255),
+`ausstate`           varchar(255),
+`telephone`          varchar(255))
+BEGIN
+declare userid int(11);
+INSERT INTO `emmet`.`users` (`username`, `firstname`, `lastname`, `email`, `activated`, `locked`) VALUES (email, firstname, lastname, email, '1', '0');
+set userid = LAST_INSERT_ID();
+INSERT INTO `emmet`.`passwords` (`userid`, `password`, `status`) VALUES (userid, password, 'CURRENT');
+INSERT INTO `emmet`.`user_role` (`user_id`, `role_id`) VALUES (userid, 'ROLE_USER');
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'city',              city);
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'organisation',      organisation);
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'primaryUserType',   primaryUserType);
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'secondaryUserType', secondaryUserType);
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'state',             ausstate);
+INSERT INTO `emmet`.`profiles` (`userid`, `property`, `value`) VALUES (userid, 'telephone',         telephone);
+END
+//
+delimiter ;
