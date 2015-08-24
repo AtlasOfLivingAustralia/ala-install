6b7f0b14a2a91addfb7500c3250d075935b16709
m-r-c <m-r-c@users.noreply.github.com>
2015-08-24 17:05:21 +1000

    Updates properties for ALA Profiles

--- ansible/roles/profile-hub/templates/delete-temp-profile-files.sh
+++ ansible/roles/profile-hub/templates/delete-temp-profile-files.sh
@@ -3,4 +3,4 @@
 #
 # Delete temporary files that are older than 7 days
 #
-find /data/profile-hub/temp -type f -mtime +7 -exec rm {} +
+sudo find /data/profile-hub/temp -type f -mtime +7 -exec rm {} +
