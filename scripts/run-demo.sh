932777d2f86e8b1c5602468dc81a4d75836a244c
Dave Martin <djtfmartin@gmail.com>
2019-10-18 13:30:27 +0100

    consolidation of elasticsearch roles to use ansible-elasticsearch - fixes for #344 specieslist - skin.fluidLayout - removed white space in default removal of localised scripts

--- scripts/run-demo.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/mar759/dev/ala-install/vagrant/ubuntu-demo
-vagrant destroy -f
-vagrant up
-ansible-playbook --private-key ~/.vagrant.d/insecure_private_key -vvvv -u vagrant -s -i /Users/mar759/dev/ala-install/ansible/inventories/vagrant/demo-vagrant /Users/mar759/dev/ala-install/ansible/ala-demo.yml
