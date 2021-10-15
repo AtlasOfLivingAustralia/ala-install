b260f25496cae43e838522fb1c6ea7d3e93743ae
Chris <chris.godwin.ala@gmail.com>
2021-10-15 12:32:31 +1100

    Increased PDF gen timeout.

--- ansible/roles/pdf-service/templates/athenapdf.sh
+++ ansible/roles/pdf-service/templates/athenapdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-export DOCKER_CMD="docker run --network host --rm -v {{pdf_path}}:/converted/ arachnysdocker/athenapdf athenapdf --delay=10000 $@"
+export DOCKER_CMD="docker run --network host --rm -v {{pdf_path}}:/converted/ arachnysdocker/athenapdf athenapdf --delay=10000 --timeout=600 $@"
 $DOCKER_CMD
