796cbbeb249ee372624b97c53fab783f3283ed75
Chris <chris.godwin.ala@gmail.com>
2022-02-08 11:43:29 +1100

    Fixed path in syncProdData.sh

--- ansible/roles/ecodata/templates/syncProdData.sh
+++ ansible/roles/ecodata/templates/syncProdData.sh
@@ -4,7 +4,7 @@ export FILE="ecodata-$(date +%y%m%d).tgz"
 
 cd /data/backups/prod
 tar -xf $FILE
-mongorestore --drop -d ecodata -u {{ ecodata_username }} -p {{ ecodata_password }} ./data/dump/ecodata
+mongorestore --drop -d ecodata -u {{ ecodata_username }} -p {{ ecodata_password }} ./data/backups/dump/ecodata
 
 mongo -u {{ ecodata_username }} -p {{ ecodata_password }} ecodata --eval 'db.setting.update({key:"meritfielddata.announcement.text"},{$set:{value:"This is the MERIT staging environment"}})';
 
