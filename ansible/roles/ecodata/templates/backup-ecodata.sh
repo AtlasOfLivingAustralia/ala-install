4841938945351afe410d67b6eeccb21b441601bb
Chris <chris.godwin.ala@gmail.com>
2022-12-18 10:06:07 +1100

    Don't include audit.json in main backup

--- ansible/roles/ecodata/templates/backup-ecodata.sh
+++ ansible/roles/ecodata/templates/backup-ecodata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p {{data_dir}}/backups
+mkdir -p {{data_dir}}/backups/audit
 cd {{data_dir}}/backups
 mongodump -d ecodata -u {{ ecodata_username }} -p {{ ecodata_password }}
+mv {{data_dir}}/backups/dump/ecodata/auditMessage.* {{data_dir}}/backups/audit/
 tar zcvf {{data_dir}}/backups/ecodata-$(date +%y%m%d).tgz {{data_dir}}/backups/dump {{data_dir}}/ecodata/models
 
