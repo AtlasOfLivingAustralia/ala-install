093d4560eaba7962174674fa969c9f3ff1bbd24f
QBai <31238531+qifeng-bai@users.noreply.github.com>
2023-11-01 09:07:27 +1100

    4.5.0 security plugin update
    4.5.0 security plugin update
    #https://github.com/AtlasOfLivingAustralia/specieslist-webapp/issues/287 lists 4.4.0 database update

--- ansible/roles/species-list/files/v4.4.0_update.sql
+++ ansible/roles/species-list/files/v4.4.0_update.sql
@@ -1,21 +1,21 @@
 #Create a matched_species table
-    CREATE TABLE `matched_species` (
-           `id` int NOT NULL AUTO_INCREMENT,
-           `taxon_concept_id` varchar(255) DEFAULT NULL,
-           `scientific_name` varchar(255) NOT NULL,
-           `scientific_name_authorship` varchar(255) DEFAULT NULL,
-           `vernacular_name` varchar(255) DEFAULT NULL,
-           `kingdom` varchar(255) DEFAULT NULL,
-           `phylum` varchar(255) DEFAULT NULL,
-           `taxon_class` varchar(255) DEFAULT NULL,
-           `taxon_order` varchar(255) DEFAULT NULL,
-           `family` varchar(255) DEFAULT NULL,
-           `genus` varchar(255) DEFAULT NULL,
-           `taxon_rank` varchar(255) DEFAULT NULL,
-           `version` BIGINT NOT NULL DEFAULT 0,
-           PRIMARY KEY (`id`,`scientific_name`),
-           UNIQUE KEY `id_UNIQUE` (`id`)
-    )
+
+        CREATE TABLE `matched_species` (
+               `id` int NOT NULL AUTO_INCREMENT,
+               `taxon_concept_id` varchar(255) DEFAULT NULL,
+               `scientific_name` varchar(255) NOT NULL,
+               `scientific_name_authorship` varchar(255) DEFAULT NULL,
+               `vernacular_name` varchar(255) DEFAULT NULL,
+               `kingdom` varchar(255) DEFAULT NULL,
+               `phylum` varchar(255) DEFAULT NULL,
+               `taxon_class` varchar(255) DEFAULT NULL,
+               `taxon_order` varchar(255) DEFAULT NULL,
+               `family` varchar(255) DEFAULT NULL,
+               `genus` varchar(255) DEFAULT NULL,
+               `taxon_rank` varchar(255) DEFAULT NULL,
+               `version` BIGINT NOT NULL DEFAULT 0,
+               PRIMARY KEY (`id`)
+        );
 
 # Link the new created matchedSpecies table to speciesListItem table:
     ALTER TABLE `specieslist`.`species_list_item`
