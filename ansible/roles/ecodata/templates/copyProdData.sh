2baebba280064f04410768122b03e9d2e7d47993
Chris <chris.godwin.ala@gmail.com>
2023-11-06 10:44:42 +1100

    Made the production server target for scripts a variable

--- ansible/roles/ecodata/templates/copyProdData.sh
+++ ansible/roles/ecodata/templates/copyProdData.sh
@@ -5,7 +5,7 @@ echo "Copying file $FILE"
 mkdir -p /data/backups/prod
 mkdir -p /data/backups/prod/audit
 cd /data/backups/prod
-scp {{ecodata_backup_user}}@ala-beyonce.it.csiro.au:/data/backups/daily/$FILE .
-scp {{ecodata_backup_user}}@ala-beyonce.it.csiro.au:/data/backups/audit/auditMessage.* ./audit/
+scp {{ecodata_backup_user}}@{{production_server_hostname}}:/data/backups/daily/$FILE .
+scp {{ecodata_backup_user}}@{{production_server_hostname}}:/data/backups/audit/auditMessage.* ./audit/
 
 
