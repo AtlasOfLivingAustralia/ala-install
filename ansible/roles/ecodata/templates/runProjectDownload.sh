829c4507b5f0dc59ab861f5c3933ba1efbb50db2
temi <temi.varghese@csiro.au>
2024-08-23 13:58:00 +1000

    AtlasOfLivingAustralia/fieldcapture#3287 - updated script to use access token instead of API - updated biocollect staging scopes - refactored secrets manager names - updated version numbers

--- ansible/roles/ecodata/templates/runProjectDownload.sh
+++ ansible/roles/ecodata/templates/runProjectDownload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source accessToken.sh
 export EMAIL=$1
 export FQ=$2
 export TABS=$3
@@ -6,7 +7,6 @@ export FORM_SECTION_PER_TAB=true
 export DOWNLOAD_URL={{merit_base_url}}/download/get/
 export SENDER_EMAIL=merit@ala.org.au
 export HUB_FQ=isMERIT%3Atrue
-export API_KEY={{ ecodata_api_key }}
 export ECODATA_BASE_URL={{ecodata_url}}/ws
 
-curl -X POST -H "Authorization: $API_KEY" "$ECODATA_BASE_URL/search/downloadAllData.xlsx?formSectionPerTab=$FORM_SECTION_PER_TAB&view=xlsx&query=*&$FQ&$TABS&hubId=$MERIT_HUB&downloadUrl=$DOWNLOAD_URL&systemEmail=$SENDER_EMAIL&senderEmail=$SENDER_EMAIL&hubFq=$HUB_FQ&email=$EMAIL&max=10000&offset=0"
+curl -X POST -H "Authorization: Bearer $access_token" "$ECODATA_BASE_URL/search/downloadAllData.xlsx?formSectionPerTab=$FORM_SECTION_PER_TAB&view=xlsx&query=*&$FQ&$TABS&hubId=$MERIT_HUB&downloadUrl=$DOWNLOAD_URL&systemEmail=$SENDER_EMAIL&senderEmail=$SENDER_EMAIL&hubFq=$HUB_FQ&email=$EMAIL&max=10000&offset=0"
